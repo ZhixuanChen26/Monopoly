84560ea022b74523fd521ef4708fb89f4b29c82a
Zhixuan Chen <diangee470399@hotmail.com>
2026-02-23 01:27:28 -0500

    Delete Monopoly/dice.cpp

--- Monopoly/dice.cpp
+++
@@ -1,9 +0,0 @@
-#include "dice.h"
-#include <random>
-
-Dice::Dice(int numSides) : sides(numSides), rng(std::random_device{}()) {}
-
-int Dice::rollDice() {
-    std::uniform_int_distribution<int> dist(1, sides);
-    return dist(rng);
-}
